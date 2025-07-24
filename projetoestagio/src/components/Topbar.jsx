0784ec0c80c8c50ca7f0c06452d74a0f945cdb4c
gustavooliveira <gustavosoliveiraem@gmail.com>
2025-07-24 12:38:56 -0300

    criando conexao com express

--- projetoestagio/src/components/Topbar.jsx
+++ projetoestagio/src/components/Topbar.jsx
@@ -6,10 +6,8 @@ const Topbar = () => (
       <img id="logo-img" src="./public/Brasao_Caratinga.png" alt="" />
     </h1>
     <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#services">Services</a>
-      <a href="#contact">Contact</a>
+      <a href="#Userlogged">Usuario ativo</a>
+
     </nav>
   </div>
 );
