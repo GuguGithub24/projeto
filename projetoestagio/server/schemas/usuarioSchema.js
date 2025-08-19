899459807caf7045f5581a1ccc221616d89a4dff
Pedro Tiago <pedro05112@gmail.com>
2025-08-19 13:39:36 -0300

    Atualizando projeto

--- projetoestagio/server/schemas/usuarioSchema.js
+++ projetoestagio/server/schemas/usuarioSchema.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const schemaUsuario = Joi.object({
   NOME_USUARIO: Joi.string().min(3).required(),
   DEPARTAMENTO: Joi.string().required(),
-  CPF: Joi.string().length(11).pattern(/^\d+$/).required(),
+  CPF: Joi.instrg().length(11).pattern(/^\d+$/).required(),
   EMAIL: Joi.string().email().required(),
   TIPO_USUARIO: Joi.string().required(),
   SENHA: Joi.string().min(6).required()
